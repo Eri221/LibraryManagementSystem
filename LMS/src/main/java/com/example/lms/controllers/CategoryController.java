a93a33ffeace46baeae5e02446f1e5ea144c6793
Erika_Vargova <vargova.erica@gmail.com>
2023-05-15 21:35:08 +0200

    adding endpoint for deleting a category

--- LMS/src/main/java/com/example/lms/controllers/CategoryController.java
+++ LMS/src/main/java/com/example/lms/controllers/CategoryController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class CategoryController {
@@ -14,10 +15,15 @@ public class CategoryController {
     this.categoryService = categoryService;
   }
 
-
   @GetMapping("/categories")
   public String findAllCategories(Model model){
     model.addAttribute("categories", categoryService.listAllCategories());
+    return "categories";
+  }
+  @GetMapping("remove-category/{id}")
+  public String deleteBook(@PathVariable Long id, Model model){
+    categoryService.deleteCategory(id);
+    model.addAttribute("categories",categoryService.listAllCategories());
     return "categories";
   }
 }
