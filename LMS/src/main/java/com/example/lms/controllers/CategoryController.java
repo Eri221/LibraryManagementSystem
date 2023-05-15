1066497eacae90754f911f483c5d159e7b47eed7
Erika_Vargova <vargova.erica@gmail.com>
2023-05-15 21:50:41 +0200

    adding endpoints for updating a category

--- LMS/src/main/java/com/example/lms/controllers/CategoryController.java
+++ LMS/src/main/java/com/example/lms/controllers/CategoryController.java
@@ -1,11 +1,15 @@
 package com.example.lms.controllers;
 
+import com.example.lms.models.Book;
+import com.example.lms.models.Category;
 import com.example.lms.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CategoryController {
@@ -25,5 +29,20 @@ public class CategoryController {
     categoryService.deleteCategory(id);
     model.addAttribute("categories",categoryService.listAllCategories());
     return "categories";
+  }
+  @GetMapping("update-category/{id}")
+  public String updateBook(@PathVariable Long id, Model model){
+    Category category = categoryService.findCategoryById(id);
+    model.addAttribute("category",category);
+    return "update-category";
+  }
+  @PostMapping("/save-update/{id}")
+  public String updateBook(@PathVariable Long id, Category category, BindingResult result, Model model){
+    if(result.hasErrors()){
+      return "update-category";
+    }
+    categoryService.updateCategory(category);
+    model.addAttribute("categories", categoryService.listAllCategories());
+    return "redirect:/categories";
   }
 }
