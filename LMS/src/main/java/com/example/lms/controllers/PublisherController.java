2dbf12259cdb7d9e196626cab91497e84e81df9d
Erika_Vargova <vargova.erica@gmail.com>
2023-05-15 23:09:00 +0200

    adding endpoint for deleting a publisher

--- LMS/src/main/java/com/example/lms/controllers/PublisherController.java
+++ LMS/src/main/java/com/example/lms/controllers/PublisherController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PublisherController {
@@ -15,6 +16,12 @@ private final PublisherService publisherService;
   }
   @GetMapping("/publishers")
   public String listAllPublishers(Model model){
+    model.addAttribute("publishers", publisherService.findAllPublishers());
+    return "publishers";
+  }
+  @GetMapping("remove-publisher/{id}")
+  public String deletePublisher(@PathVariable Long id, Model model){
+    publisherService.deletePublisher(id);
     model.addAttribute("publishers", publisherService.findAllPublishers());
     return "publishers";
   }
